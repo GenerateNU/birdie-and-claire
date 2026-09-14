9795facecfaf4a32039627ca53bae65fb026aa65
Abhishek Vijay <vijay.ab@northeastern.edu>
2026-09-14 19:11:07 -0400

    db query complete

--- starwars-api/db/db.go
+++ starwars-api/db/db.go
@@ -85,6 +85,24 @@ func GetCharactersByFaction(faction string) ([]models.Character, error) {
 	//   - check the error from DB.Query
 	//   - defer rows.Close()
 	//   - check rows.Err() after the loop
-
-	return nil, nil
+	rows, err := DB.Query("SELECT id, name, species, faction, force_sensitive, power_level FROM characters WHERE faction = ?", faction)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var characters []models.Character
+	for rows.Next() {
+		var c models.Character
+		var fs int
+		err := rows.Scan(&c.ID, &c.Name, &c.Species, &c.Faction, &fs, &c.PowerLevel)
+		if err != nil {
+			return nil, err
+		}
+		c.ForceSensitive = fs == 1
+		characters = append(characters, c)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return characters, nil
 }
